0ff406a5ae9eb56a01a94c600f2815fb2a59d723
jabber <2898117012@qq.com>
2023-08-29 02:23:46 +0800

    init

--- se-age-gate.php
+++ se-age-gate.php
@@ -1,7 +1,16 @@
 <?php
-/*
-Plugin Name: SHOPEO Age Gate
-Description: A lightweight, customisable age gate to lock content from younger audience.
-Version: 0.0.1
+/**
+ * Plugin Name: SHOPEO Age Gate
+ * Plugin URI:  https://woocommerce.com/products/se-age-gate/
+ * Description: A lightweight, customisable age gate to lock content from younger audience.
+ * Version:     0.0.1
+ * Author:      SHOPEO
+ * Author URI:  https://shopeo.cn/
+ * License:     GPL-2.0
+ * Text Domain: se-age-gate
+ */
 
-*/
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
